34e1ed49e8fa462cd2ce6a826c2a4d2cad5dd4c4
ViktorJT <v.jensentorp@gmail.com>
2025-03-22 22:38:56 +0100

    feat: add base tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,66 @@
 /** @type {import('tailwindcss').Config} */
+
+const baseTypographyStyles = {
+  lineHeight: "125%",
+  letterSpacing: "-0.5px",
+};
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#00703A",
+        "off-white": "#FAFAFA",
+      },
+      fontFamily: {
+        sans: ['"SF Pro"', "sans-serif"],
+      },
+      typography: {
+        display: {
+          ...baseTypographyStyles,
+          fontSize: "40px",
+        },
+        h1: {
+          ...baseTypographyStyles,
+          fontSize: "24px",
+        },
+        title: {
+          ...baseTypographyStyles,
+          fontSize: "14px",
+        },
+        subtitle: {
+          ...baseTypographyStyles,
+          fontSize: "12px",
+          fontWeight: "600",
+          textTransform: "uppercase",
+          color: "rgba(0, 0, 0, 0.4)",
+        },
+        body: {
+          ...baseTypographyStyles,
+          fontSize: "12px",
+        },
+      },
+      boxShadow: {
+        card: `-4px 2px 10px 0px #00000005,
+               -16px 9px 18px 0px #00000005,
+               -35px 20px 24px 0px #00000000,
+               -63px 36px 29px 0px #00000000,
+               -98px 56px 32px 0px #00000000`,
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const typographyStyles = {};
+      const typographyTheme = theme("typography") || {};
+      Object.entries(typographyTheme).forEach(([name, styles]) => {
+        typographyStyles[`.text-${name}`] = styles;
+      });
+      addUtilities(typographyStyles);
+    },
+  ],
 };
