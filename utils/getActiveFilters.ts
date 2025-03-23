9c5dd8e0c992e75b0673c7376bece264f69bacbb
ViktorJT <v.jensentorp@gmail.com>
2025-03-23 23:35:28 +0100

    fix: incorrect naming of food category

--- utils/getActiveFilters.ts
+++ utils/getActiveFilters.ts
@@ -2,7 +2,7 @@ import type { SearchParams } from "@/types/page";
 
 export function getActiveFilters(searchParams: SearchParams) {
   return {
-    food: new Set(searchParams.category?.split(",").filter(Boolean) ?? []),
+    food: new Set(searchParams.food?.split(",").filter(Boolean) ?? []),
     price: new Set(searchParams.price?.split(",").filter(Boolean) ?? []),
     duration: new Set(searchParams.duration?.split(",").filter(Boolean) ?? []),
   };
