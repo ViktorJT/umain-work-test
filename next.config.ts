0230f84920fbb67e59747f17a2e8f2606ac27da6
ViktorJT <v.jensentorp@gmail.com>
2025-03-23 11:36:40 +0100

    fix: add placeholder source to safe hosts in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        search: "",
+      },
+    ],
+  },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule: any) =>
       rule.test?.test?.(".svg"),
