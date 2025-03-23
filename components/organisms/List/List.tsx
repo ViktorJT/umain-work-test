9f1e2d1d7a1951e108e2c5e93fed23368b8adced
ViktorJT <v.jensentorp@gmail.com>
2025-03-23 13:56:22 +0100

    feat: List server component

---
+++ components/organisms/List/List.tsx
@@ -0,0 +1,45 @@
+import { cx } from "class-variance-authority";
+
+import { getRestaurantsWithOpenStatus } from "@/data/getRestaurantsWithOpenStatus";
+
+import { Card } from "@/components/molecules/Card";
+
+import type { Props } from "./List.types";
+
+export default async function List(props: Props) {
+  const restaurants = await getRestaurantsWithOpenStatus();
+
+  return (
+    <div
+      className={cx(
+        `
+          grid  
+          grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+          auto-rows-[200px]
+          gap-1 px-4
+        `,
+        props.className,
+      )}
+    >
+      {restaurants.map((restaurant) => {
+        return (
+          <Card
+            key={restaurant.id}
+            title={restaurant.name}
+            image={{
+              src: restaurant.image_url,
+              alt: restaurant.name,
+            }}
+            isOpen={restaurant.isOpen}
+            deliveryTime={`${restaurant.delivery_time_minutes} min`}
+            button={{
+              href: `/restaurants/${restaurant.id}`,
+              ariaLabel: `Go to ${restaurant.name}`,
+              icon: { name: "arrow-right" },
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
