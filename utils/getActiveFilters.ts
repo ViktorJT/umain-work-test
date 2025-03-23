a570d45119774aebf15395508ab3bbfc479ce62b
ViktorJT <v.jensentorp@gmail.com>
2025-03-24 00:08:29 +0100

    fix: naming

--- utils/getActiveFilters.ts
+++ utils/getActiveFilters.ts
@@ -1,9 +1,10 @@
+import type { ActiveFiltersByCategory } from "@/types/filter";
 import type { SearchParams } from "@/types/page";
 
-export function getActiveFilters(searchParams: SearchParams) {
-  return {
-    food: new Set(searchParams.food?.split(",").filter(Boolean) ?? []),
-    price: new Set(searchParams.price?.split(",").filter(Boolean) ?? []),
-    duration: new Set(searchParams.duration?.split(",").filter(Boolean) ?? []),
-  };
-}
+export const getActiveFilters = (
+  searchParams: SearchParams,
+): ActiveFiltersByCategory => ({
+  food: new Set(searchParams.food?.split(",").filter(Boolean) ?? []),
+  price: new Set(searchParams.price?.split(",").filter(Boolean) ?? []),
+  duration: new Set(searchParams.duration?.split(",").filter(Boolean) ?? []),
+});
