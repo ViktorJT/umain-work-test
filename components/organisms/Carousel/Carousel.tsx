d26056d42c4b718f0f7e6518726b7e7f3f211b6e
ViktorJT <v.jensentorp@gmail.com>
2025-03-23 16:36:29 +0100

    feat: add Carousel component w. mock data

---
+++ components/organisms/Carousel/Carousel.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Tile } from "@/components/atoms/Tile";
+
+import { categories } from "@/data/mock";
+
+export default function Carousel() {
+  return (
+    <section
+      className={`
+        w-full overflow-x-auto px-4
+        scroll-smooth snap-x scroll-pl-4
+      `}
+    >
+      <div className="flex gap-3 py-2 min-w-max pr-4">
+        {categories.map(({ label, image }) => (
+          <Tile key={label} label={label} image={image} />
+        ))}
+      </div>
+    </section>
+  );
+}
