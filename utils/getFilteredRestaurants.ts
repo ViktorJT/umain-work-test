eb1339042c2f66f70e5027735f8ca1e9e63a97d1
ViktorJT <v.jensentorp@gmail.com>
2025-03-24 12:09:16 +0100

    feat: AND filtering instead of OR

--- utils/getFilteredRestaurants.ts
+++ utils/getFilteredRestaurants.ts
@@ -12,19 +12,31 @@ export function getFilteredRestaurants({
   restaurants,
   activeFiltersByCategory,
 }: Props) {
-  const allActiveFilters = new Set(
-    Object.values(activeFiltersByCategory).flatMap((groupSet) =>
-      Array.from(groupSet),
-    ),
-  );
-
-  const filteredRestaurants = allActiveFilters.size
+  const filteredRestaurants = Object.values(activeFiltersByCategory).some(
+    (set) => set.size > 0,
+  )
     ? restaurants.filter(
         ({ filter_ids, delivery_time_minutes, price_range_id }) => {
           const delivery_time_id = getDeliveryTimeId(delivery_time_minutes);
 
-          return [...filter_ids, delivery_time_id, price_range_id].some((id) =>
-            allActiveFilters.has(id),
+          return Object.entries(activeFiltersByCategory).every(
+            ([group, activeSet]) => {
+              if (activeSet.size === 0) return true;
+
+              if (group === "food") {
+                return filter_ids.some((id) => activeSet.has(id));
+              }
+
+              if (group === "price") {
+                return activeSet.has(price_range_id);
+              }
+
+              if (group === "duration") {
+                return activeSet.has(delivery_time_id);
+              }
+
+              return true;
+            },
           );
         },
       )
